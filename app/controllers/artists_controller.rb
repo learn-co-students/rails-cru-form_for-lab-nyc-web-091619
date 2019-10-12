70f754c25638afd9fc054ded2b37a6c6ac5c16d5
maryfarner18 <maryfarner@gmail.com>
2019-10-12 15:17:15 -0400

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,34 @@
+class ArtistsController < ApplicationController
+
+    def index
+        @artists = Artist.all
+    end
+
+    def new
+        @artist = Artist.new
+    end
+
+    def create 
+        @artist = Artist.create(params.require(:artist).permit(:name, :bio))
+
+        redirect_to artist_path(@artist)
+    end
+
+    def show
+        @artist = Artist.find(params[:id])
+        @songs = @artist.songs
+        @genres = @artist.genres
+    end
+
+    def edit
+        @artist = Artist.find(params[:id])
+    end
+
+    def update 
+        @artist = Artist.find(params[:id])
+        @artist.update(params.require(:artist).permit(:name, :bio))
+        
+        redirect_to artist_path(@artist)
+    end
+    
+end
