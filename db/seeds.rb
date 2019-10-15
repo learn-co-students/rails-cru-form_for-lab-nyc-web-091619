9c78f79a1636fa6c58713b4553e7dc6617fb310b
Anya <anya.s.litvinova@gmail.com>
2019-10-15 11:40:15 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+kanye = Artist.create(name: "Kanye", bio: "I'm the Yeezy")
+britney = Artist.create(name:"Briney Spears", bio: "It's Britney, b..ch!")
+elton = Artist.create(name:"Elton John", bio:"Call me Sir")
+
+hip_hop = Genre.create(name:"hip hop")
+pop = Genre.create(name:"pop")
+rock = Genre.create(name: rock)
+
+# stronger = Song.create(kanye.id, hip_hop.id)
+# oops = Song.create(britney.id, pop.id)
+# leon = Song.create(elton.id, rock.id)
