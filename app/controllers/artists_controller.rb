9cd11c3ad9d54bc90f496a7216fd7a91d75af195
dan1mal <dreamer@panix.com>
2019-10-14 22:34:06 -0400

    updates.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,35 @@
+class ArtistsController < ApplicationController
+    
+    def new 
+        @artist = Artist.new
+    end
+    
+    def show
+        @artist = Artist.find(params[:id])
+    end
+
+    def create
+        @artist = Artist.new(artist_params)
+        
+        @artist.save
+        redirect_to artist_path(@artist)
+    end
+
+    def edit 
+        @artist = Artist.find(params[:id])
+    end
+
+    def update
+        @artist = Artist.find(params[:id])
+        # @artist.update(params.require(:artist).permit(:name, :bio))
+        @artist.update(artist_params)
+        redirect_to artist_path
+    end
+
+    private
+
+        def artist_params
+            params.require(:artist).permit(:name, :bio)
+        end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :artists, only: [:create, :edit, :new, :show, :update] 
+  resources :genres, only: [:create, :new, :show, :update] 
+  resources :songs, only: [:create, :new, :show, :update] 
 end
